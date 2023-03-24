f08a9b03849ff0c5af15f8e6d3c2febc51976d70
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2023-03-25 00:49:34 +0530

    Routing part done.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var lodash = require('lodash');
+const { toLower } = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,23 +13,70 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+var posts=[];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",(req,res)=>{
+  res.render("home",{
+    title:"Home",
+    content:homeStartingContent,
+    posts:posts
 
+  });
+});
 
+app.get("/about",(req,res)=>{
+  res.render("about",{
+    title:"About",
+    content:aboutContent
+    
+  });
+});
 
+app.get("/contact",(req,res)=>{
+  res.render("contact",{
+    title:"Conatct",
+    content:contactContent
+  });
 
+});
 
+app.get("/compose",(req,res)=>{
+  res.render("compose",{
+    title:"Compose",
 
+  });
+});
 
+app.post("/compose",(req,res)=>{
+  var postTitle=req.body.postTitle;
+  var postBody=req.body.postBody;
 
+  const post={
+    title:postTitle,
+    body:postBody
+  };
+  
+  posts.push(post);
+  res.redirect("/");
+});
 
-
-
-
+app.get("/post/:postTopic",(req,res)=>{
+  var postTopic=lodash.lowerCase(req.params.postTopic);
+  posts.forEach((post)=>{
+    if(lodash.lowerCase(post.title)===postTopic){
+      console.log("match found!");
+      res.render("post",{
+        title:post.title,
+        body:post.body
+      })
+    }
+  })
+})
 
 
 app.listen(3000, function() {
